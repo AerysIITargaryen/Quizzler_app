20fe9e7fc0c4a3a298b42b71edaa081bf4318eb6
Ivan Stankin <stankin93@icloud.com>
2023-07-31 22:21:39 +0300

    Initial Commit

---
+++ Quizzler_app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Quizzler_app
+//
+//  Created by Иван Станкин on 31.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
